3844e842dbd238b240ef9fc81028d18d9dc2ff73
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2025-04-10 13:25:33 +0100

    just fixed typo

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,11 +22,6 @@ connectDB()
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-const app: Express = express();
-const PORT = process.env.PORT || 5000;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
     app.use(helloRouter);
     app.use("/auth", authRouter);
     app.use("/products", productRouter);
